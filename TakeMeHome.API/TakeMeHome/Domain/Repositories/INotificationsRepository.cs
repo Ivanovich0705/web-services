9bbec81eefc284d130777ad5f3f9b7bb8203bf22
FaridIzquierdo <u202015215@upc.edu.pe>
2022-11-11 21:50:34 -0500

    feat(notifications)
    Controllers,Domain,Mapping,Percistence,Resources,Services

---
+++ TakeMeHome.API/TakeMeHome/Domain/Repositories/INotificationsRepository.cs
@@ -0,0 +1,13 @@
+﻿using TakeMeHome.API.TakeMeHome.Domain.Models;
+
+namespace TakeMeHome.API.TakeMeHome.Domain.Repositories;
+
+public interface INotificationsRepository
+{
+    Task<IEnumerable<Notifications>> ListAsync();
+    Task AddAsync(Notifications notifications);
+    Task<Notifications> FindIdAsync(int id);
+    Task<IEnumerable<Notifications>> FindByUserId(int userId);
+    void Update(Notifications notifications);
+    void Remove(Notifications notifications);
+}
---
+++ TakeMeHome.API/TakeMeHome/Resources/NotificationsResource.cs
@@ -0,0 +1,8 @@
+﻿namespace TakeMeHome.API.TakeMeHome.Resources;
+
+public class NotificationsResource
+{
+    public int Id { get; set; }
+    
+    public string Type { get; set; }
+}
