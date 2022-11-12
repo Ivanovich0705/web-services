84f7716b901924670e5510ecb4420c84ff10687e
Ivanovich0705 <ivantenis1@icloud.com>
2022-11-11 20:37:19 -0500

    Dependencias and Directories created

--- TakeMeHome.API/Program.cs
+++ TakeMeHome.API/Program.cs
@@ -1,13 +1,69 @@
+using System.Configuration;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.EntityFrameworkCore;
+using TakeMeHome.API.TakeMeHome.Domain.Models;
+using TakeMeHome.API.TakeMeHome.Domain.Repositories;
+using TakeMeHome.API.TakeMeHome.Domain.Services;
+using TakeMeHome.API.TakeMeHome.Mapping;
+using TakeMeHome.API.TakeMeHome.Persistence.Contexts;
+using TakeMeHome.API.TakeMeHome.Persistence.Repositories;
+using TakeMeHome.API.TakeMeHome.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+builder.Services.AddSwaggerGen(c =>
+{
+//c.SwaggerDoc("v1");
+}).AddSwaggerGenNewtonsoftSupport();
+
+//Add Database Connection
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors());
+
+//Add lower case routes
+builder.Services.AddRouting(options=> options.LowercaseUrls = true);
+
+//Dependency Injection Configuration
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOrderStatusRepository, OrderStatusRepository>();
+builder.Services.AddScoped<IOrderStatusService, OrderStatusService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<INotificationsService, NotificationsService>();
+builder.Services.AddScoped<INotificationsRepository, NotificationsRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+//AutoMapper Configuration
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProfile), 
+    typeof(ResourceToModelProfile));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
