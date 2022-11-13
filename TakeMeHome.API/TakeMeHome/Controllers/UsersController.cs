44cde4acc74f5fc8a3105a61de543c3dee41d1c2
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2022-11-13 00:50:18 -0500

    feat(users):  Added swagger documentation.

--- TakeMeHome.API/TakeMeHome/Controllers/UsersController.cs
+++ TakeMeHome.API/TakeMeHome/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using TakeMeHome.API.Shared.Extensions;
 using TakeMeHome.API.TakeMeHome.Domain.Models;
 using TakeMeHome.API.TakeMeHome.Domain.Services;
@@ -8,7 +9,9 @@ using TakeMeHome.API.TakeMeHome.Resources;
 
 namespace TakeMeHome.API.TakeMeHome.Controllers;
 //Method Controller for Users
+[ApiController]
 [Route("/api/v1/[controller]")]
+[Produces("application/json")]
 public class UsersController : ControllerBase
 {
     private readonly IUserService _userService;
@@ -21,6 +24,15 @@ public class UsersController : ControllerBase
     }
     
     [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get all users",
+        Description = "List of all the users in the database",
+        OperationId = "GetAllUsers"
+        )]
+    [SwaggerResponse(200, "The operation was successful", typeof(IEnumerable<CommentResource>))]
+    [SwaggerResponse(400, "The data is not valid")]
+    [SwaggerResponse(404, "Users were not found")]
+    [SwaggerResponse(500, "An unexpected error occurred")]
     public async Task<IEnumerable<UserResource>> GetAllAsync()
     {
         var users = await _userService.ListAsync();
@@ -28,8 +40,17 @@ public class UsersController : ControllerBase
         return resources;
     }
     
-    [HttpGet]
-    [Route("{id}")]
+    [HttpGet("{id}")]
+    [SwaggerOperation(
+        Summary = "Get a user by id",
+        Description = "Get a user by id",
+        OperationId = "GetUserById"
+        )]
+    
+    [SwaggerResponse(200, "The operation was successful", typeof(UserResource))]
+    [SwaggerResponse(400, "The data is not valid")]
+    [SwaggerResponse(404, "User was not found")]
+    [SwaggerResponse(500, "An unexpected error occurred")]
     public async Task<IActionResult> FindByIdAsync(int id)
     {
         var result = await _userService.FindByIdAsync(id);
@@ -38,9 +59,19 @@ public class UsersController : ControllerBase
         var resource = _mapper.Map<User, UserResource>(result);
         return Ok(resource);
     }
+    
  
     [HttpGet]
     [Route("{email}/{password}")]
+    [SwaggerOperation(
+        Summary = "Get user by email and password",
+        Description = "Get a specific user by email and password",
+        OperationId = "FindByEmailAndPasswordAsync"
+    )]
+    [SwaggerResponse(200, "The operation was successful", typeof(CommentResource))]
+    [SwaggerResponse(400, "The data is not valid")]
+    [SwaggerResponse(404, "User was not found")]
+    [SwaggerResponse(500, "An unexpected error occurred")]
     public async Task<IActionResult> FindByEmailAndPasswordAsync(string email, string password)
     {
         var result = await _userService.FindByEmailAndPasswordAsync(email, password);
@@ -52,6 +83,15 @@ public class UsersController : ControllerBase
     
     [HttpGet]
     [Route("username={username}")]
+    [SwaggerOperation(
+        Summary = "Get user by username",
+        Description = "Get a specific user by username",
+        OperationId = "FindByUsernameAsync"
+    )]
+    [SwaggerResponse(200, "The operation was successful", typeof(CommentResource))]
+    [SwaggerResponse(400, "The data is not valid")]
+    [SwaggerResponse(404, "User was not found")]
+    [SwaggerResponse(500, "An unexpected error occurred")]
     public async Task<IActionResult> FindByUsernameAsync(string username)
     {
         var result = await _userService.FindByUserNameAsync(username);
@@ -62,6 +102,14 @@ public class UsersController : ControllerBase
     }
 
     [HttpPost]
+    [SwaggerOperation(
+        Summary = "Create a new user",
+        Description = "Create a new user",
+        OperationId = "CreateUser"
+    )]
+    [SwaggerResponse(200, "The operation was successful", typeof(UserResource))]
+    [SwaggerResponse(400, "The data is not valid")]
+    [SwaggerResponse(500, "An unexpected error occurred")]
     public async Task<IActionResult> PostAsync([FromBody] SaveUserResource resource)
     {
         if (!ModelState.IsValid)
@@ -78,6 +126,14 @@ public class UsersController : ControllerBase
     }
     
     [HttpDelete("{id}")]
+    [SwaggerOperation(
+        Summary = "Delete a user",
+        Description = "Delete a user",
+        OperationId = "DeleteUser"
+    )]
+    [SwaggerResponse(200, "The operation was successful", typeof(UserResource))]
+    [SwaggerResponse(400, "The data is not valid")]
+    [SwaggerResponse(500, "An unexpected error occurred")]
     public async Task<IActionResult> DeleteAsync(int id)
     {
         var result = await _userService.DeleteAsync(id);
@@ -90,6 +146,14 @@ public class UsersController : ControllerBase
     }
     
     [HttpPut("{id}")]
+    [SwaggerOperation(
+        Summary = "Update a user",
+        Description = "Update a user",
+        OperationId = "UpdateUser"
+    )]
+    [SwaggerResponse(200, "The operation was successful", typeof(UserResource))]
+    [SwaggerResponse(400, "The data is not valid")]
+    [SwaggerResponse(500, "An unexpected error occurred")]
     public async Task<IActionResult> PutAsync(int id, [FromBody] SaveUserResource resource)
     {
         if (!ModelState.IsValid)
@@ -106,6 +170,14 @@ public class UsersController : ControllerBase
     }
     
     [HttpPatch("{id}")]
+    [SwaggerOperation(
+        Summary = "Update specific user attribute",
+        Description = "Update specific user attribute",
+        OperationId = "UpdateUser"
+    )]
+    [SwaggerResponse(200, "The operation was successful", typeof(UserResource))]
+    [SwaggerResponse(400, "The data is not valid")]
+    [SwaggerResponse(500, "An unexpected error occurred")]
     public async Task<IActionResult> UpdateAsync(int id, [FromBody] JsonPatchDocument<User> resource)
     {
         var user = await _userService.FindByIdAsync(id);
