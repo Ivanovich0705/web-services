6605eaf52a436f402a4c0efe903f338e7a8211b7
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2022-11-17 14:58:54 -0500

    feat(orders): Added get by status and user id method in the repository interface.

--- TakeMeHome.API/TakeMeHome/Domain/Repositories/IOrderRepository.cs
+++ TakeMeHome.API/TakeMeHome/Domain/Repositories/IOrderRepository.cs
@@ -8,7 +8,8 @@ public interface IOrderRepository
     Task AddAsync(Order order);
     Task<Order> FindByIdAsync(int id);
     Task<IEnumerable<Order>> FindByOrderStatusId(int orderStatusId);
-    Task<IEnumerable<Order>> FindyByUserId(int userId);
+    Task<IEnumerable<Order>> FindByOrderStatusIdAndUserId(int orderStatusId, int userId);
+     Task<IEnumerable<Order>> FindyByUserId(int userId);
     void Update(Order order);
     void Remove(Order order);
 }
