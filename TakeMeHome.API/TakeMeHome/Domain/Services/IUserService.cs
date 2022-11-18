75e3a6eae7f831b73afd2dac67ca52122caf4486
Anthony Botello S <89035933+AnthonyBotello4@users.noreply.github.com>
2022-11-17 21:05:43 -0500

    feat(users): added endpoint to check if a email already exists

--- TakeMeHome.API/TakeMeHome/Domain/Services/IUserService.cs
+++ TakeMeHome.API/TakeMeHome/Domain/Services/IUserService.cs
@@ -9,6 +9,7 @@ public interface IUserService
     Task<User> FindByIdAsync(int id);
     Task<User> FindByUserNameAsync(string userName);
     Task<User> FindByEmailAndPasswordAsync(string email, string password);
+    Task<User> FindByEmailAsync(string email);
     Task<UserResponse> SaveAsync(User user);
     Task<UserResponse> UpdateAsync(int id, User user);
     Task<UserResponse> DeleteAsync(int id);
